161b76d9734386c436ceb326ce60a14151062efc
Prakhar <Prakhar.Lad@iiitb.ac.in>
2022-04-10 16:06:51 +0530

    Initial commit

---
+++ src/test/java/com/iiitb/scientificCalc/ScientificCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiitb.scientificCalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScientificCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
